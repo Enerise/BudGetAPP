17a5e5c5d58c395d2dab5652d4ae6f4270e1ec6d
Magdalena Brodala <magdalena.brodala.programista@gmail.com>
2023-09-27 23:47:12 +0200

    add functions with password reset

--- App/Models/User.php
+++ App/Models/User.php
@@ -144,4 +144,40 @@ class User extends \Core\Model
 
         return $stmt->execute();
     }
+
+    public static function sendPasswordReset($email)
+    {
+        $user = static::findByEmail($email);
+
+        if ($user) {
+
+            if ($user->startPasswordReset()) {
+
+                // Send email here...                
+
+            }
+        }
+    }
+
+    protected function startPasswordReset()
+    {
+        $token = new Token();
+        $hashed_token = $token->getHash();
+
+        $expiry_timestamp = time() + 60 * 60 * 2;  // 2 hours from now
+
+        $sql = 'UPDATE users
+                SET password_reset_hash = :token_hash,
+                    password_reset_expires_at = :expires_at
+                WHERE id = :id';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':token_hash', $hashed_token, PDO::PARAM_STR);
+        $stmt->bindValue(':expires_at', date('Y-m-d H:i:s', $expiry_timestamp), PDO::PARAM_STR);
+        $stmt->bindValue(':id', $this->id, PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
 }
